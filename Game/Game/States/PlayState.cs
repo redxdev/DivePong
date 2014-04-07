158b554e0deb6fdd1372ea83667b7e54ed2623cd
Sam Bloomberg <sam@redxdev.com>
2014-04-06 23:10:42 -0400

    more project setup

---
+++ Game/Game/States/PlayState.cs
@@ -0,0 +1,12 @@
+﻿namespace Pong.Game.States
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PlayState
+    {
+    }
+}
